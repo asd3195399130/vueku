d4a6b35fe7fa9b721a885eb18410b3ae19924902
杨浩冰 <3195399130@qq.com>
2023-08-04 23:19:10 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App ！！！"/>
+  <div id="app">  
+   <Code></Code>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Code from './components/xiaochengxu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Code
   }
 }
 </script>
